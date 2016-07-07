0c0ca8948c787f5562e4f9425b896d833ac0072f
AndyCodez <andystudy4@gmail.com>
2016-07-07 16:38:39 +0300

    Enable Login using username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      redirect_to user
+    else
+      render 'new'
+    end
   end
 end
