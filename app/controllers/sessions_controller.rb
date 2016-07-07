4b38e1b11825312707d6c9b0a85b41ea4a87dacb
AndyCodez <andystudy4@gmail.com>
2016-07-07 17:30:36 +0300

    Store user's id in temporary cookie using the 'session' method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
+      log_in user
       redirect_to user
     else
       flash.now[:alert] = "Invalid username/password combination"
