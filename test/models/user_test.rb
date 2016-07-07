8d75bf05928c52af14e503bf99dbecd18e9bcfed
AndyCodez <andystudy4@gmail.com>
2016-07-07 14:51:27 +0300

    Add User.digest method to User model and test user validity

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.new( username: "Makatunga", email: "makatunga@makatunga.com", 
+                      password: "cartoon", password_confirmation: "cartoon")
+  end
+
+  test "user should be valid" do
+    assert @user.valid?
+  end
 end
