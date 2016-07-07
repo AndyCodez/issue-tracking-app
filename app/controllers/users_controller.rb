6ca63a4fe36329053794ca933ecd10621aba13a4
AndyCodez <andystudy4@gmail.com>
2016-07-07 13:51:49 +0300

    Flash successfully signed up user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,23 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
 
+      flash[:success] = "You have successfully registered!"
+      redirect_to @user 
+      
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 end
