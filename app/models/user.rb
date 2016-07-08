236bda56010b37a346c72461df6a25f0da1fedc5
AndyCodez <andystudy4@gmail.com>
2016-07-09 01:26:17 +0300

    Test and validate name and email length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :email, presence: true
+  validates :username, presence: true, length: { maximum: 30}
+
+  validates :email, presence: true, length: { maximum: 255 }
 
   has_secure_password
 
