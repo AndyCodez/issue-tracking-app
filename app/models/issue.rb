2b5489751e651112176693f4135d5b85b34b3261
AndyCodez <andystudy4@gmail.com>
2016-07-20 22:21:57 +0300

    Add status to issues

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,7 @@
 class Issue < ApplicationRecord
   belongs_to :user
   enum priority: { low: 0, medium: 1, high: 2 }
+  enum status: { open: 0, in_progress: 1, resolved: 2, closed: 3 }
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :title, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720084417) do
+ActiveRecord::Schema.define(version: 20160720190904) do
 
   create_table "issues", force: :cascade do |t|
     t.text     "description"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160720084417) do
     t.datetime "updated_at",              null: false
     t.integer  "priority",    default: 0
     t.string   "title"
+    t.integer  "status",      default: 0
     t.index ["user_id", "created_at"], name: "index_issues_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_issues_on_user_id"
   end
