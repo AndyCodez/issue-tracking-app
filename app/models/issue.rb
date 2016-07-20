454b4b5886d75ca27636df3e46fce18f15b982c9
AndyCodez <andystudy4@gmail.com>
2016-07-20 17:27:32 +0300

    Validate title presence to be true

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,6 +3,7 @@ class Issue < ApplicationRecord
   enum priority: { low: 0, medium: 1, high: 2 }
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
+  validates :title, presence: true
   validates :description, presence: true, length: { minimum: 50 }
   validates :priority, presence: true
 
