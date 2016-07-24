51a42bb902a9094d7bbf1070d7c919db6c5a1934
AndyCodez <andystudy4@gmail.com>
2016-07-25 01:53:58 +0300

    Edit form content

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
         #Allow access
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user) 
-        redirect_to new_issue_path
+        redirect_to user
       end
     else
       flash.now[:alert] = "Invalid username/password combination"
