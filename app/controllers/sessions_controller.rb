1b81dee2abe5c1c7ed67bbefa0c3d3cec3acd8c4
AndyCodez <andystudy4@gmail.com>
2016-08-20 02:32:31 +0300

    Redirect admin to all issues path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,11 @@ class SessionsController < ApplicationController
         #Allow access
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user) 
-        redirect_to user
+        if current_user.admin?
+          redirect_to issues_path
+        else
+          redirect_to user
+        end
       end
     else
       flash.now[:alert] = "Invalid username/password combination"
